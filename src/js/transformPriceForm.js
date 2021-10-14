1b8226c9ae68e280821c5d27a2047a25d278cc60
Ahn-GiHwan <ahngh0113@naver.com>
2021-10-14 20:31:05 +0900

    feat: move /js to /js/function

--- src/js/transformPriceForm.js
+++
@@ -1,6 +0,0 @@
-export const transformPriceForm = (num, str = "") => {
-  // return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + "원";
-  return num.toLocaleString("ko-KR") + str;
-};
-
-export default transformPriceForm;
